d1a3e537c8e7fba81b56df575d0613148dfda4b5
Emil Fresk <emil.fresk@gmail.com>
2020-03-16 22:37:58 +0100

    Save

--- elf_test/src/main.rs
+++ elf_test/src/main.rs
@@ -1,18 +1,14 @@
 use std::fs;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use xmas_elf::{sections::SectionData, symbol_table::Entry, ElfFile};
+use xmas_elf::{
+    sections::{SectionData, SHF_ALLOC},
+    symbol_table::Entry,
+    ElfFile,
+};
 
 #[derive(StructOpt)]
 struct Opts {
-    // #[structopt(short, long, parse(try_from_str = parse_hex))]
-    // vendor: u16,
-
-    // #[structopt(short, long, parse(try_from_str = parse_hex))]
-    // product: u16,
-
-    // #[structopt(long)]
-    // verify: bool,
     #[structopt(long, parse(from_os_str))]
     elf: PathBuf,
 }
@@ -25,14 +21,21 @@ fn main() -> Result<(), anyhow::Error> {
     let elf = &ElfFile::new(&bytes).map_err(anyhow::Error::msg)?;
 
     for sect in elf.section_iter() {
+        if sect.flags() & SHF_ALLOC != 0 {
+            println!("alloc section: {:?}", sect.get_name(elf));
+        } else {
+            println!("not alloc section: {:?}", sect.get_name(elf));
+        }
+
         if sect.get_name(elf) == Ok(".symtab") {
             if let Ok(symtab) = sect.get_data(elf) {
                 if let SectionData::SymbolTable32(entries) = symtab {
                     for entry in entries {
                         if let Ok(name) = entry.get_name(elf) {
+                            println!("names: {}", rustc_demangle::demangle(name).to_string());
                             if name == "TEST" {
                                 println!(
-                                    "Found '{}', address = 0x{:8x}, size = {}b",
+                                    "        Found '{}', address = 0x{:8x}, size = {}b",
                                     name,
                                     entry.value(),
                                     entry.size()
