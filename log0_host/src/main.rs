2148b6952bc29ab0dac79132e8e8dd7cc920a335
Emil Fresk <emil.fresk@gmail.com>
2020-04-10 18:34:42 +0200

    More testing

--- log0_host/src/main.rs
+++ log0_host/src/main.rs
@@ -73,13 +73,14 @@ fn main() -> Result<(), anyhow::Error> {
 
     // Get a list of all available debug probes.
     let probes = Probe::list_all();
-    println!("probes: {:#?}", probes);
+    // println!("probes: {:#?}", probes);
 
     // Use the first probe found.
     let mut probe = probes[0].open()?;
     // println!("probe: {:#?}", probe);
     probe.select_protocol(WireProtocol::Swd)?;
-    probe.set_speed(2_000)?;
+    let speed_khz = probe.set_speed(24_000)?;
+    println!("Probe speed: {} kHz", speed_khz);
 
     // Attach to a chip.
     let session = probe.attach("stm32l412cbu")?;
@@ -96,7 +97,7 @@ fn main() -> Result<(), anyhow::Error> {
         Format::Elf,
         DownloadOptions {
             progress: Some(&FlashProgress::new(|event| {
-                println!("event: {:#?}", event);
+                // println!("event: {:#?}", event);
             })),
             keep_unwritten_bytes: false,
         },
@@ -115,7 +116,8 @@ fn main() -> Result<(), anyhow::Error> {
 
     // Read a single 32 bit word.
     let mut old_target = 0;
-    let mut read_buff = vec![0; buf_address.unwrap().1];
+    let buf_size = buf_address.unwrap().1;
+    let mut read_buff = vec![0; buf_size];
 
     loop {
         let mut buff = [0u32; 2];
@@ -130,19 +132,22 @@ fn main() -> Result<(), anyhow::Error> {
         if target != old_target {
             old_target = target;
 
-            let br = bytes_to_read(host as usize, target as usize, 1024);
+            let br = bytes_to_read(host as usize, target as usize, buf_size);
 
             let mut read = &mut read_buff[0..br];
 
-            if host + br as u32 > 1024 {
+            if host + br as u32 > buf_size as u32 {
                 // cursor will overflow
-                let pivot = host.wrapping_add(br as u32).wrapping_sub(1024) as usize;
+                let pivot = host.wrapping_add(br as u32).wrapping_sub(buf_size as u32) as usize;
                 core.read_8(buf_address.unwrap().0 + host, &mut read[0..pivot])?;
                 core.read_8(buf_address.unwrap().0, &mut read[pivot..br])?;
                 core.write_word_32(cursor_address.unwrap() + 4, (br - pivot) as u32)?;
             } else {
                 core.read_8(buf_address.unwrap().0 + host, &mut read)?;
-                core.write_word_32(cursor_address.unwrap() + 4, (host + br as u32) % 1024)?;
+                core.write_word_32(
+                    cursor_address.unwrap() + 4,
+                    (host + br as u32) % buf_size as u32,
+                )?;
             }
             let dur = now.elapsed();
 
@@ -162,7 +167,7 @@ fn main() -> Result<(), anyhow::Error> {
 
 fn bytes_to_read(host_idx: usize, target_idx: usize, buffer_size: usize) -> usize {
     // let cursor = host_idx;
-    let len_to_read = (target_idx - host_idx + buffer_size) % buffer_size;
+    let len_to_read = target_idx.wrapping_sub(host_idx).wrapping_add(buffer_size) % buffer_size;
 
     return len_to_read;
 
